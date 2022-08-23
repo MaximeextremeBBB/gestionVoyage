ccd3b7f3343852076f90ac78072c06058232f0bf
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2022-08-23 09:38:35 +0200

    first commit

---
+++ src/main/java/com/inti/ProjetavecKevin/entities/Hotel.java
@@ -0,0 +1,41 @@
+package com.inti.ProjetavecKevin.entities;
+
+public class Hotel {
+
+	private Long idHotel;
+	private String nom;
+	private int nbEtoile;
+	
+	public Hotel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getIdHotel() {
+		return idHotel;
+	}
+
+	public void setIdHotel(Long idHotel) {
+		this.idHotel = idHotel;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getNbEtoile() {
+		return nbEtoile;
+	}
+
+	public void setNbEtoile(int nbEtoile) {
+		this.nbEtoile = nbEtoile;
+	}
+
+	
+	
+	
+}
---
+++ src/test/java/com/inti/ProjetavecKevin/ProjetavecKevinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.ProjetavecKevin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetavecKevinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
